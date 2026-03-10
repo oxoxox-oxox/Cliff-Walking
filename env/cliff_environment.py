26d41c3b3480931d6239eb28b41ed1b0d7c3d8ff
oxoxox-oxox <jiabaocao5@gmail.com>
2026-03-10 16:54:08 +0800

    feat: add environment part of cliff walking

---
+++ env/cliff_environment.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+class CliffWalkingEnv:
+
+    def __init__(self):
+        """
+        Cliff Walking Environment
+
+        Grid size: 4 * 12
+
+        S = Start
+        G = Goal
+        C = Cliff
+        """
+
+        self.row = 4
+        self.col = 12
+
+        self.start = (3, 0)
+        self.end = (3, 11)
+
+        self.pos = self.start
+
+        self.action = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        self.cliff = [(3, i) for i in range(1, 11)]
+
+    def move(self, actions):
+        """
+        Let the model move and update its position 
+        """
+
+        x, y = self.pos
+        dx, dy = self.action[actions]
+
+        nx = x + dx
+        ny = y + dy
+
+        nx = max(0, min(self.row-1, nx))
+        ny = max(0, min(self.col-1, ny))
+
+        if (nx, ny) in self.cliff:
+            reward = -100
+            done = True
+
+            self.pos = self.start
+
+        elif (nx, ny) in self.end:
+            reward = 0
+            done = True
+
+        else:
+            reward = -1
+            done = False
+
+        return self._get_state(), reward, done
+
+    def reset(self):
+        """
+        Reset at every episode
+        """
+
+        self.pos = self.start
+
+    def _get_state(self):
+        """
+        Change (x,y) into state index
+        """
+
+        x, y = self.pos
+        return x * self.row + self.col
+
+    def render(self):
+        """
+        Print the current environment
+        """
