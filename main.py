5b82194d8d2a44a143e34abbcc9a471fde4d4f35
oxoxox-oxox <jiabaocao5@gmail.com>
2026-03-10 19:35:35 +0800

    feat: add main and q_learning for the project

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-import numpy as np
-import torch
+from env import cliff_environment
+from agent import q_learning
 
-env = [[0,0,0,0,0,0,0,0,0,0],
-       [0.0.0.0.0.0.0.0.0.0],
-       [0.0.0.0.0.0.0.0.0.0],
-       [1.0.0.0.0.0.0.0.0.-1]]
+env = cliff_environment()
 
+n_states = env.row * env.col
+n_actions = 4
 
+agent = q_learning(n_states, n_actions)
 
+episodes = 500
 
+for episode in range(episodes):
+
+    state = env.reset()
+    done = False
+
+    total_reward = 0
+
+    while not done:
+
+        action = agent.choose_action(state)
+
+        next_state, reward, done = env.step(action)
+
+        agent.update(state, action, reward, next_state)
+
+        state = next_state
+
+        total_reward += reward
+
+    print("episode", episode, "reward", total_reward)
